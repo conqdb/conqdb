989efdcf435405a956e92346a3120f8c9a8848ea
Harley Salas <harley@harleysalas.com>
2024-09-23 23:00:44 +0300

    fix cached preview image

--- apps/web/src/payload/collections/Unit/components/UnitImagePreview.tsx
+++ apps/web/src/payload/collections/Unit/components/UnitImagePreview.tsx
@@ -1,49 +1,41 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { useFormFields } from '@payloadcms/ui'
-import * as qs from 'qs-esm'
 import './index.scss'
 type Props = {
   serverURL: string
 }
 
 export const UnitImagePreview = ({ serverURL }: Props) => {
-  const { value: imageId } = useFormFields(([fields]) => fields.image)
+  const image = useFormFields(([fields]) => fields.image)
   const [url, setUrl] = useState<string | null>(null)
 
   useEffect(() => {
     const fetchUrl = async () => {
-      const query = {
-        depth: 0,
-        draft: true,
-        id: imageId,
-      }
-
-      const response = await fetch(`${serverURL}/api/media`, {
-        body: qs.stringify(query),
+      const response = await fetch(`${serverURL}/api/media/${image.value}?draft=true&depth=0`, {
         credentials: 'include',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
-          'X-HTTP-Method-Override': 'GET',
         },
-        method: 'POST',
+        method: 'GET',
       })
 
       if (response.ok) {
         const json = await response.json()
-        const string = json.docs[0]?.url
+        const string = json.url
         setUrl(string)
       } else {
         setUrl(null)
       }
     }
 
-    if (imageId && typeof imageId === 'string') {
+    if (image.value && typeof image.value === 'string') {
       fetchUrl()
     } else {
       setUrl(null)
     }
-  }, [imageId, setUrl])
+    return () => setUrl(null)
+  }, [image, setUrl, serverURL])
 
   const { value: x } = useFormFields(([fields]) => fields['imageSettings.x'])
   const { value: y } = useFormFields(([fields]) => fields['imageSettings.y'])
@@ -55,6 +47,7 @@ export const UnitImagePreview = ({ serverURL }: Props) => {
         <div className="field-label">Preview</div>
         <div className="unit-image-preview__card">
           <img
+            key={image.value as string}
             src={url}
             className="unit-image-preview__image"
             style={{ transform: `translateX(${x}%) translateY(${y}%) scale(${scale})` }}
